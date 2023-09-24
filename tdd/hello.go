65c6d4a38cd6cdc428c83a57dbaccb8fd32c34ad
Matias Vega <mativega@8amazon.com>
2023-09-24 06:46:42 -0700

    multi language hello world

--- tdd/hello.go
+++ tdd/hello.go
@@ -2,15 +2,31 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
-
-func main() {
-	fmt.Println(Hello("world"))
+type LangConstants struct {
+	prefix,	suffix string
 }
 
-func Hello(name string) string {
+func main() {
+	fmt.Println(Hello("world", "en"))
+}
+
+func Hello(name string, lang string) string {
+
+	langMap := map[string]LangConstants{
+		"en": {"Hello, ", "World"},
+		"es": {"Hola, ", "Mundo"},
+		"fr": {"Bonjour, ", "Monde"},
+	}
+	
+	c, ok := langMap[lang]
+
+	if !ok {
+		 c = langMap["en"]
+	}
+
 	if name == "" {
-		name = "World"
-	} 
-	return englishHelloPrefix + name
+		name = c.suffix
+	}
+
+	return c.prefix + name
 }
--- tdd/hello_test.go
+++ tdd/hello_test.go
@@ -4,14 +4,34 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Matias")
+		got := Hello("Matias", "en")
 		want := "Hello, Matias"
 
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "en")
 		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("", "es")
+		want := "Hola, Mundo"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("unknown language say hi in english", func(t *testing.T) {
+		got := Hello("", "xx")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("", "fr")
+		want := "Bonjour, Monde"
 
 		assertCorrectMessage(t, got, want)
 	})
